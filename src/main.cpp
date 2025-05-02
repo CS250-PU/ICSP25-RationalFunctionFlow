3848f58400cd041bc8c5e2bc36ddf81b8b93eeac
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2025-05-02 18:00:11 +0000

    In class program flow complete

--- src/main.cpp
+++ src/main.cpp
@@ -13,54 +13,65 @@
 using namespace std;
 
 class Rational {
-  public:
-    Rational () { cout << "ctor1" << endl; mNumerator = 0; mDenominator = 1;}
-    Rational (int numerator, int denominator) {
-      cout << "ctor2" << endl;
-      mNumerator = numerator;
-      mDenominator = denominator;
-    } 
-    Rational (const Rational &rational) {
-      cout << "ctor3" << endl;
-      mNumerator = rational.mNumerator;
-      mDenominator = rational.mDenominator;
-    }
-    ~Rational () { cout << "dtor" << endl;}
-    Rational &operator= (const Rational &rational) {
-      cout << "op=" << endl;
-      if (this != &rational) {
-        mNumerator = rational.mNumerator;
-        mDenominator = rational.mDenominator;
-      }
-      return *this;
-    }
-    friend ostream &operator<< (ostream &os, const Rational &rational) {
-      os << rational.mNumerator << "/" << rational.mDenominator;
-      return os;
-    }
+public:
+  Rational () {
+    cout << "ctor1 *this: " << *this << endl;
+    mNumerator = 0; mDenominator = 1;
+  }
 
-  private:
-    int mNumerator;
-    int mDenominator;
+  Rational (int numerator, int denominator) {
+    mNumerator = numerator;
+    mDenominator = denominator;
+    cout << "ctor2 *this: " << *this << endl;
+  }
+
+  Rational (const Rational& rcRational) {
+    mNumerator = rcRational.mNumerator;
+    mDenominator = rcRational.mDenominator;
+    cout << "ctor3 *this: " << *this << endl;
+  }
+
+  ~Rational () {
+    cout << "dtor *this: " << *this << endl;
+  }
+
+  Rational& operator= (const Rational& rcRational) {
+    cout << "op= *this: " << *this << "rcRational: " << rcRational << endl;
+    if (this != &rcRational) {
+      mNumerator = rcRational.mNumerator;
+      mDenominator = rcRational.mDenominator;
+    }
+    return *this;
+  }
+
+  friend ostream& operator<< (ostream& rcOutStream,
+    const Rational& rcRational) {
+    rcOutStream << rcRational.mNumerator << "/" << rcRational.mDenominator;
+    return rcOutStream;
+  }
+
+private:
+  int mNumerator;
+  int mDenominator;
 };
 
 void foo (Rational cR) {
   cout << "foo" << endl;
 }
 
-Rational bar (const Rational &cR) {
-  cout << "bar" << endl;
+Rational bar (const Rational& cR) {
+  cout << "bar cR: " << cR << endl;
   return cR;
 }
 
 int main () {
   Rational cR1;
-  Rational cR2(1, 2);
-  Rational cR3(cR2);
+  Rational cR2 (1, 2);
+  Rational cR3 (cR2);
   Rational cR4 = cR2;
   Rational cR5 (2, 3);;
   cout << "cR1: " << cR1 << endl;
-  (cR1 = bar(cR2)) = cR5;
+  (cR1 = bar (cR2)) = cR5;
   cout << "cR1: " << cR1 << endl;
 
   std::cout << "Process Completed" << std::endl;
